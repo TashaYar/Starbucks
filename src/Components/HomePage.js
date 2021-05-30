1d999030a27b21b9c5977a1c8e958d8e1d79b991
Pallavi Singh <pgs909294@gmail.com>
2021-05-30 21:13:14 +0530

    Third

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -8,6 +8,11 @@ import card1Img from "../Assets/about1.jpg";
 import card2Img from "../Assets/about2.jpg";
 import card3Img from "../Assets/about3.jpg";
 import card4Img from "../Assets/about4.jpg";
+import caret from "../Assets/caretUp.svg";
+import facebook from "../Assets/facebook.svg";
+import instagram from "../Assets/instagram.svg";
+import pinterest from "../Assets/pinterest.svg";
+import twitter from "../Assets/twitter.svg";
 
 export class HomePage extends Component {
   constructor(props) {
@@ -15,6 +20,7 @@ export class HomePage extends Component {
 
     this.state = {
       visibleNav: true,
+      activeAccord: 0,
     };
   }
   onScrollFunc = (e) => {
@@ -32,6 +38,7 @@ export class HomePage extends Component {
     window.addEventListener("scroll", this.onScrollFunc);
     window.scrollTo(0, 0);
   }
+
   render() {
     return (
       <div>
@@ -53,17 +60,7 @@ export class HomePage extends Component {
               }}
             />
           </Col>
-          <Button
-            className="btn-1"
-            style={
-              {
-                /*               transform: this.state.visibleNav ? "" : "translate(2px,-2px)",
-                 */
-              }
-            }
-          >
-            How to order your Starbucks®
-          </Button>
+          <Button className="btn-1">How to order your Starbucks®</Button>
           <Col
             className="col-12 col-md-8 secondSide "
             style={{
@@ -73,7 +70,7 @@ export class HomePage extends Component {
             <img alt="upImage" src={upImg} className="upImg" />
             <div className="col-12  thisSummer">
               <div className="thisSummerContent">
-                <h2 className="h1">This summer it starts with you</h2>
+                <h2 className="h1">This Summer It Starts With You</h2>
                 <p className="p1">
                   {" "}
                   Chocolate? ✓ Coffee? ✓ Crunch? ✓ Sip summer with our new
@@ -102,14 +99,21 @@ export class HomePage extends Component {
               >
                 <img alt="desc1" src={desc1} className="desc1" />
               </div>
-              <div>
-                <h3> Starbucks® Rewards</h3>
-                <p>
+              <div className="descContent">
+                <h3 style={{ textAlign: "start", fontWeight: "bold" }}>
+                  {" "}
+                  Starbucks® Rewards
+                </h3>
+                <p style={{ textAlign: "start" }}>
                   With the Starbucks® UK app, you can order on-the-go. Perfect
                   for those long summer days!
                 </p>
-                <p>This Summer It Starts With You.</p>
-                <p>Order ahead and bag rewards.</p>
+                <p style={{ textAlign: "start" }}>
+                  This Summer It Starts With You.
+                </p>
+                <p style={{ textAlign: "start" }}>
+                  Order ahead and bag rewards.
+                </p>
                 <Button className="btn-card">Find out more</Button>
               </div>
             </div>
@@ -123,11 +127,11 @@ export class HomePage extends Component {
                       className="cardimage"
                     />
                     <Card.Body>
-                      <Card.Title>
+                      <Card.Title style={{ textAlign: "start" }}>
                         {" "}
                         Order ahead with the Starbucks® UK app
                       </Card.Title>
-                      <Card.Text>
+                      <Card.Text style={{ textAlign: "start" }}>
                         Skip the queue and get Rewards when you order on our
                         app.
                       </Card.Text>
@@ -143,8 +147,10 @@ export class HomePage extends Component {
                       className="cardimage"
                     />
                     <Card.Body>
-                      <Card.Title>Starbucks® Delivers</Card.Title>
-                      <Card.Text>
+                      <Card.Title style={{ textAlign: "start" }}>
+                        Starbucks® Delivers
+                      </Card.Title>
+                      <Card.Text style={{ textAlign: "start" }}>
                         Get your favourite food and drinks delivered do your
                         door!
                       </Card.Text>
@@ -162,8 +168,10 @@ export class HomePage extends Component {
                       className="cardimage"
                     />
                     <Card.Body>
-                      <Card.Title>About Us</Card.Title>
-                      <Card.Text>
+                      <Card.Title style={{ textAlign: "start" }}>
+                        About Us
+                      </Card.Title>
+                      <Card.Text style={{ textAlign: "start" }}>
                         Find out more about our company and heritage.
                       </Card.Text>
                       <Button className="btn-card">Find out more</Button>
@@ -178,14 +186,215 @@ export class HomePage extends Component {
                       className="cardimage"
                     />
                     <Card.Body>
-                      <Card.Title>Starbucks Stories</Card.Title>
-                      <Card.Text>
+                      <Card.Title style={{ textAlign: "start" }}>
+                        Starbucks Stories
+                      </Card.Title>
+                      <Card.Text style={{ textAlign: "start" }}>
                         Keep up to date with our latest stories and news.
                       </Card.Text>
                       <Button className="btn-card">Find out more</Button>
                     </Card.Body>
                   </Card>
                 </div>
+              </div>
+            </div>
+            <div className="footerPart">
+              <button
+                className="accordion"
+                onClick={(e) => {
+                  if (this.state.activeAccord !== 1)
+                    this.setState({ activeAccord: 1 });
+                  else {
+                    this.setState({ activeAccord: 0 });
+                  }
+                }}
+              >
+                About Us
+                <img
+                  src={caret}
+                  alt="caret"
+                  className="accordion-img"
+                  style={{
+                    transform:
+                      this.state.activeAccord === 1 ? "rotate(0deg)" : "",
+                  }}
+                />
+              </button>
+              <div
+                className="panel"
+                style={{
+                  maxHeight: this.state.activeAccord === 1 ? "21rem" : "0",
+                }}
+              >
+                <ul className="accordianLinks">
+                  <a href="#" className="footlink">
+                    About Us
+                  </a>
+                  <a href="#" className="footlink">
+                    Our Coffees
+                  </a>
+                  <a href="#" className="footlink">
+                    Starbucks Stories & News
+                  </a>
+                  <a href="#" className="footlink">
+                    Starbucks® Ready to Drink
+                  </a>
+                  <a href="#" className="footlink">
+                    Foodservice Coffee
+                  </a>
+                  <a
+                    href="#"
+                    className="footlink"
+                    style={{ marginBottom: "10px" }}
+                  >
+                    Customer Service
+                  </a>
+                </ul>
+              </div>
+
+              <button
+                className="accordion"
+                onClick={(e) => {
+                  if (this.state.activeAccord !== 2)
+                    this.setState({ activeAccord: 2 });
+                  else {
+                    this.setState({ activeAccord: 0 });
+                  }
+                }}
+              >
+                Careers
+                <img
+                  src={caret}
+                  className="accordion-img"
+                  alt="caret"
+                  style={{
+                    transform:
+                      this.state.activeAccord === 2 ? "rotate(0deg)" : "",
+                  }}
+                />
+              </button>
+              <div
+                className="panel"
+                style={{
+                  maxHeight: this.state.activeAccord === 2 ? "5rem" : "0",
+                }}
+              >
+                <ul className="accordianLinks">
+                  <a
+                    href="#"
+                    className="footlink"
+                    style={{ marginBottom: "10px" }}
+                  >
+                    Search Careers
+                  </a>
+                </ul>
+              </div>
+
+              <button
+                className="accordion"
+                onClick={(e) => {
+                  if (this.state.activeAccord !== 3)
+                    this.setState({ activeAccord: 3 });
+                  else {
+                    this.setState({ activeAccord: 0 });
+                  }
+                }}
+              >
+                Social Impact
+                <img
+                  src={caret}
+                  alt="caret"
+                  className="accordion-img"
+                  style={{
+                    transform:
+                      this.state.activeAccord === 3 ? "rotate(0deg)" : "",
+                  }}
+                />
+              </button>
+              <div
+                className="panel"
+                style={{
+                  maxHeight: this.state.activeAccord === 3 ? "10rem" : "0",
+                }}
+              >
+                <ul className="accordianLinks">
+                  <a href="#" className="footlink">
+                    Responsibility
+                  </a>
+
+                  <a
+                    href="#"
+                    className="footlink"
+                    style={{ marginBottom: "10px" }}
+                  >
+                    Covid-19
+                  </a>
+                </ul>
+              </div>
+              <button
+                className="accordion"
+                onClick={(e) => {
+                  if (this.state.activeAccord !== 4)
+                    this.setState({ activeAccord: 4 });
+                  else {
+                    this.setState({ activeAccord: 0 });
+                  }
+                }}
+              >
+                Starbucks Card
+                <img
+                  src={caret}
+                  alt="caret"
+                  className="accordion-img"
+                  style={{
+                    transform:
+                      this.state.activeAccord === 4 ? "rotate(0deg)" : "",
+                  }}
+                />
+              </button>
+              <div
+                className="panel"
+                style={{
+                  maxHeight: this.state.activeAccord === 4 ? "10rem" : "0",
+                }}
+              >
+                <ul className="accordianLinks">
+                  <a href="#" className="footlink">
+                    Starbucks Card
+                  </a>
+                  <a href="#" className="footlink">
+                    Starbucks Card Terms & Conditions
+                  </a>
+                </ul>
+              </div>
+              <hr className="horizontal-line"></hr>
+              <div className="socialMedias">
+                <a href="#">
+                  <img src={facebook} className="socialLink" />
+                </a>
+                <a href="#">
+                  <img src={pinterest} className="socialLink" />
+                </a>
+                <a href="#">
+                  <img src={instagram} className="socialLink" />
+                </a>
+                <a href="#">
+                  <img src={twitter} className="socialLink" />
+                </a>
+              </div>
+              <div className="footerEnd">
+                <a href="#" className="lastFootlink">
+                  Contact Us
+                </a>
+                <a href="#" className="lastFootlink">
+                  Privacy Policy
+                </a>
+                <a href="#" className="lastFootlink">
+                  Terms of Use
+                </a>
+                <a href="#" className="lastFootlink">
+                  Cookie Policy
+                </a>
               </div>
             </div>
           </Col>
--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import HomePage from "./HomePage";
-const pallavi = "Pallavi";
-const gadahi = true;
+
 class MainComponent extends Component {
   render() {
     return (
